baca4e7b32c67574f383469434dfe5d43f245fae
brassardfelix <brassardfelix@gmail.com>
2024-02-05 16:44:21 -0500

    Imported BEDPE

--- BRASS_analysis/microhomology_readFinder.py
+++ BRASS_analysis/microhomology_readFinder.py
@@ -2,6 +2,11 @@ import pandas as pd
 import pysam
 
 
-### Test sample ###
+### TEST SAMPLE ###
+### Assumes that we are located at /home/racinef/projects/def-jacquesp/racinef/BRASS_bedpe of Beluga server
+### Import bam file
 samfile = pysam.AlignmentFile('../../cancer/HAP1/HRN_T6_C12_2-1707693.bam', "rb" )
+### Import bedpe file for sample
+bedpe = pd.read_csv('HRN_T6_C12_2-1707693.annot.bedpe', sep='\t') 
+
 
