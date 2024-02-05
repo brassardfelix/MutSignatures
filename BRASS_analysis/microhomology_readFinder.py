b714848ada99389f9818b7de3fa2cfdb7f984f64
brassardfelix <brassardfelix@gmail.com>
2024-02-05 16:45:08 -0500

    Test commit

--- BRASS_analysis/microhomology_readFinder.py
+++ BRASS_analysis/microhomology_readFinder.py
@@ -7,6 +7,6 @@ import pysam
 ### Import bam file
 samfile = pysam.AlignmentFile('../../cancer/HAP1/HRN_T6_C12_2-1707693.bam', "rb" )
 ### Import bedpe file for sample
-bedpe = pd.read_csv('HRN_T6_C12_2-1707693.annot.bedpe', sep='\t') 
+bedpe = pd.read_csv('HRN_T6_C12_2-1707693.annot.bedpe', sep='\t')   ## this is for the test commit
 
 
